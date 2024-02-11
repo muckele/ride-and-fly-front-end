6aca39252450f394ba62c757413a46dd9c25aa94
Mathew <muckele7@gmail.com>
2024-02-11 14:38:55 -0500

    creates edit button on profile page

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,6 +1,6 @@
 // npm modules
 import { useState, useEffect } from 'react'
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 // services
 import * as profileService from '../../services/profileService'
@@ -8,7 +8,7 @@ import * as profileService from '../../services/profileService'
 // css
 import styles from './Profiles.module.css'
 
-const Profiles = () => {
+const Profiles = (props) => {
   const { profileId } = useParams()
   // const [profiles, setProfiles] = useState([])
   const [userProfile, setUserProfile] = useState(null)
@@ -29,7 +29,9 @@ const Profiles = () => {
     <main className={styles.container}>
       <h1>Profile</h1>
       <p>{userProfile.name}</p>
+      <Link to={`/profiles/${profileId}/edit`} state={props.user}>EDIT</Link>
     </main>
+  
   )
 }
 
