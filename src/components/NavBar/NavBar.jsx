4390feef32cc19977eff701b1fa385f0ee7351ac
Mathew <muckele7@gmail.com>
2024-02-12 16:59:55 -0500

    adds trips to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,10 +13,10 @@ const NavBar = ({ user, handleLogout }) => {
           <li><NavLink to={`/profiles/${user.profile}`}>Profiles</NavLink></li>
           {/* <li><NavLink to="/profiles/:profileId">Profiles</NavLink></li> */}
           <li><NavLink to="/posts">Posts</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
           <li><NavLink to="/posts/new">New Post</NavLink></li>
-          <li><NavLink to="/inbox">Inbox</NavLink></li>
+          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+          <li><NavLink to="/trips">Trips</NavLink></li>
+          <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
         </ul>
       :
         <ul>
