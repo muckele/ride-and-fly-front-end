adbd24ed0ed6c58290fec0a3c4b5d51921849713
Jia Wang <daisy-a.119@hotmail.com>
2024-02-13 00:13:25 -0500

    add more nav styling and landi ng page styling

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,10 +3,10 @@ import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
+    <main className='container'>
       <h1>hello, {user ? user.name : 'friend'}</h1>
       <div className='container-left'>
-        <div className='bold-font'>
+        <div className='content'>
           <h1>SHARE JOURNEY<br/>SHARE FUN</h1>
           <p>Ride & FLy is your hassel-free solution to rider and share your way to airport. This is the app where you can share your journey and fun with fellow buddies as well as the expenses.</p>
         </div>
