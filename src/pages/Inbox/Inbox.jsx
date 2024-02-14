c72c2270aae768b4565a318f2aff6fce0bec6ec1
Berlin <tommyonik@gmail.com>
2024-02-14 17:00:11 -0500

    refactor inbox to show conversations and successfully show data

--- src/pages/Inbox/Inbox.jsx
+++ src/pages/Inbox/Inbox.jsx
@@ -12,7 +12,7 @@ const Inbox = (props) => {
     <div className='inbox-container'>
       <h1>Inbox</h1>
       {props.conversations.map((conversation) => (
-        <ConversationCard key={conversation._id} convo={conversation}/>
+        <ConversationCard key={conversation._id} conversation={conversation}/>
       ))}
       
     </div>
