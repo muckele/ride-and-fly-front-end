f027ce7a13103b8a81d472cddf5d63e076aa31df
Mathew <muckele7@gmail.com>
2024-02-15 12:16:25 -0500

    fixes trip list

--- src/pages/TripList/TripList.jsx
+++ src/pages/TripList/TripList.jsx
@@ -1,7 +1,5 @@
 // npm modules
-import { useState } from "react"
-
-
+import { useState, useEffect } from "react"
 
 // components
 import TripCard from '../../components/TripCard/TripCard'
@@ -9,20 +7,42 @@ import TripCard from '../../components/TripCard/TripCard'
 // css
 import './TripList.css'
 
+
+
 const TripList = (props) => {
+  const [activeTrips, setActiveTrips] = useState([])
+  const [completedTrips, setCompletedTrips] = useState([])
+
+  useEffect(() => {
+    const active = props.trips.filter(trip => trip.isActive)
+    const completed = props.trips.filter(trip => !trip.isActive)
+    setActiveTrips(active)
+    setCompletedTrips(completed)
+  }, [props.trips])
+
+  const completeTrip = (tripd) => {
+    const trip = activeTrips.find(t => t._id === tripId)
+    if (!trip) return
+    const updatedActiveTrips = activeTrips.filter(t => t._id !== tripId);
+    setActiveTrips(updatedActiveTrips)
+    setCompletedTrips([...completedTrips, { ...trip, isActive: false }])
+  }
+
   console.log(props.trips)
   return (
     <>
       <main>
         <div className="active-trips">
-            <h1>Active Trips</h1>
-            {props.trips.map((trip) => (
-              <TripCard key={trip._id} trip={trip}  />
-            ))}
-            {/* <button onClick={handleCompleteTrip}>Click Here</button> */}
+          <h2>Active Trips</h2>
+          {activeTrips.map(trip => (
+            <TripCard key={trip._id} trip={trip} onComplete={() => completeTrip(trip._id)} />
+          ))}
         </div>
         <div className="completed-trips">
-          <h1>Completed Trips</h1>
+          <h2>Completed Trips</h2>
+          {completedTrips.map(trip => (
+            <TripCard key={trip._id} trip={trip} />
+          ))}
         </div>
       </main>
     </>
@@ -30,3 +50,10 @@ const TripList = (props) => {
 }
 
 export default TripList
+
+
+            // <h1>Active Trips</h1>
+            // {props.trips.map((trip) => (
+            //   <TripCard key={trip._id} trip={trip}  />
+            // ))}
+            // {/* <button onClick={handleCompleteTrip}>Click Here</button> */}
