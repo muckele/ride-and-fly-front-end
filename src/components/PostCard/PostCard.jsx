992c8e698a13b9138ea46572750596846a211b88
Mathew <muckele7@gmail.com>
2024-02-16 12:34:38 -0500

    styling edits

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -6,14 +6,13 @@ import './PostCard.css'
 
 const PostCard = ( {post} ) => {
 
-
   return (
     <div className='outer-postcard-container'>
         <Link to={`/posts/${post._id}`}>
       <div className='postcard-container'>
       <ul>
         <li>Car Pal: {post.author[0].name}</li>
-        <li>Date/Time: {post.date}</li>
+        <li>Date: {post.date}</li>
         <li>Time: {post.time}</li>
         <li>Airport: {post.airport}</li>
         <li>Terminal: {post.terminal}</li>
