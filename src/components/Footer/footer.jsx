78cdd2824bd170977222ff00c914fac666711b51
Mathew <muckele7@gmail.com>
2024-03-26 13:51:56 -0400

    adds github informtion to our footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (  
     <div className='footer-container'>
       <div id="footer-p">
-      <p className='footer-p'>©2024 Copyright by Ride & Fly . Made with 💙 by Tommy, Matt, Jia & Asa </p> <br/>
+      <p className='footer-p'>©2024 Copyright by Ride & Fly . Made with 💙 by <a href="https://github.com/TheBerlinMan">Tommy</a>, <a href="https://github.com/muckele">Mathew</a>, <a href="https://github.com/daisy119">Jia</a> & <a href="https://github.com/Asagutierrez">Asa</a> </p> <br/>
     </div>
     </div>
   );
