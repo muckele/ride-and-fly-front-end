8eab7a8a1978a694ab9d5bec89ddc327da53d325
Mathew <muckele7@gmail.com>
2024-06-09 12:24:05 -0400

    fixes date on post card page

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -5,6 +5,11 @@ import { Link } from 'react-router-dom'
 import './PostCard.css'
 
 const PostCard = ( {post} ) => {
+  const formattedDate = new Date(post.date).toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
 
   return (
     <div className='outer-postcard-container'>
@@ -12,7 +17,7 @@ const PostCard = ( {post} ) => {
       <div className='postcard-container'>
       <ul>
         <li>Car Pal: {post.author[0].name}</li>
-        <li>Date: {post.date}</li>
+        <li>Date: {formattedDate}</li>
         <li>Time: {post.time}</li>
         <li>Airport: {post.airport}</li>
         <li>Terminal: {post.terminal}</li>
