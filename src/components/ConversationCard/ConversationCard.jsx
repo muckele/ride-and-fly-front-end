4c6dee459ac650f6e3e5dd03f535781a218d3f3d
Mathew <muckele7@gmail.com>
2024-06-12 20:52:14 -0400

    fixes date formating in inbox

--- src/components/ConversationCard/ConversationCard.jsx
+++ src/components/ConversationCard/ConversationCard.jsx
@@ -1,25 +1,27 @@
-//npm modules
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-//css
-import '../ConversationCard/ConversationCard.css'
+import '../ConversationCard/ConversationCard.css';
 
-const ConversationCard = ( { conversation , user} ) => {
+const ConversationCard = ({ conversation, user }) => {
+  const [formattedDate, setFormattedDate] = useState('');
 
-  
+  useEffect(() => {
+    if (conversation.createdAt) {
+      const date = new Date(conversation.createdAt);
+      const options = { month: 'long', day: 'numeric', year: 'numeric' };
+      setFormattedDate(date.toLocaleDateString('en-US', options));
+    }
+  }, [conversation.createdAt]);
 
-  return ( 
-
+  return (
     <div className='conversation-card'>
       <Link to={`/conversations/${conversation._id}`}>
-        
-        <p>Created: {conversation.createdAt}</p>
-        <p>From:{(user.profile === conversation.messageAuthor._id) ? "Me" : conversation.messageAuthor.name}</p>
+        <p>Created: {formattedDate}</p>
+        <p>From: {(user.profile === conversation.messageAuthor._id) ? "Me" : conversation.messageAuthor.name}</p>
         <p>To: {(user.profile === conversation.recipient._id) ? "Me" : conversation.recipient.name}</p>
-      
       </Link>
     </div>
-
-  )
-}
+  );
+};
 
 export default ConversationCard;
